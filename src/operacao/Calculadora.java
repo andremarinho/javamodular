190f9e29008bb31b395f9b42ccb39a7d2db30943
andremarinho <andre.bmarinho@hotmail.com>
2021-06-09 12:29:40 -0300

    Implementacao calculadora

--- src/operacao/Calculadora.java
+++ src/operacao/Calculadora.java
@@ -1,4 +1,38 @@
 package operacao;
 
+import operacao.internal.DivHelper;
+import operacao.internal.MulHelper;
+import operacao.internal.SubHelper;
+import operacao.internal.SumHelper;
+
 public class Calculadora {
+
+    private SumHelper sumHelper;
+    private SubHelper subHelper;
+    private DivHelper divHelper;
+    private MulHelper mulHelper;
+
+    public Calculadora(){
+        subHelper = new SubHelper();
+        sumHelper = new SumHelper();
+        divHelper = new DivHelper();
+        mulHelper = new MulHelper();
+    }
+
+    public int sum(int a, int b){
+        return this.sumHelper.execute(a,b);
+    }
+
+    public int sub(int a, int b){
+        return this.subHelper.execute(a,b);
+    }
+
+    public int mul(int a, int b){
+        return this.mulHelper.execute(a,b);
+    }
+
+    public int div(int a, int b){
+        return this.subHelper.execute(a,b);
+    }
+
 }
